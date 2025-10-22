2b63efb4508c29ff4448a278f59bfd00f2145466
about18k <kimytac20@gmail.com>
2025-10-22 08:38:31 +0800

    WIP: local changes before syncing upstream

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,18 @@
+const nativewindBabel = require("react-native-css-interop/babel");
+
 module.exports = function (api) {
   api.cache(true);
+
+  const nativewindConfig = nativewindBabel();
+  const filteredPlugins = (nativewindConfig.plugins ?? []).filter((plugin) => {
+    if (typeof plugin === "string") {
+      return plugin !== "react-native-worklets/plugin";
+    }
+    return true;
+  });
+
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
+    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+    plugins: filteredPlugins,
   };
 };
